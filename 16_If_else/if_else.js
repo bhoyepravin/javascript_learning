a74a371c7c79316fd77061fea0bbab0424e6e2aa
bhoyepravin <bhoyepravin288@gmail.com>
2025-07-30 23:50:54 +0530

    if_else,switch statement

---
+++ 16_If_else/if_else.js
@@ -0,0 +1,42 @@
+// if
+// else if
+// else if
+// else if
+// else
+
+// if else means if the condition is true then do this else do this
+// if one condition is satisfied then it will not check the other conditions
+
+// let tempInDegree = 50;
+
+// if (tempInDegree < 0) {
+//   console.log("extremely cold outside");
+// } else if (tempInDegree < 16) {
+//   console.log("cold outside");
+// } else if (tempInDegree < 25) {
+//   console.log("nice outside");
+// } else if (tempInDegree < 35) {
+//   console.log("let's go for swim");
+// } else if (tempInDegree < 45) {
+//   console.log("extremely warm outside");
+// } else {
+//   console.log("too hot!!");
+// }
+
+// console.log("Ok");
+
+let tempInDegree = 45;
+
+if (tempInDegree > 40) {
+  console.log("too hot");
+} else if (tempInDegree > 30) {
+  console.log("lets go for swim");
+} else if (tempInDegree > 20) {
+  console.log("wheater is cool");
+} else if (tempInDegree > 10) {
+  console.log("its very cold outside");
+} else {
+  console.log("extremely cold");
+}
+
+console.log("Hello");
---
+++ 17_Switch_Statement/01_Switch_statement.js
@@ -0,0 +1,54 @@
+// switch statement
+
+// if print day 1 sunday
+// print day 2 monday
+
+// we make using if else
+// let day = 9;
+
+// if (day === 0) {
+//   console.log("Sunday");
+// } else if (day === 1) {
+//   console.log("Monday");
+// } else if (day === 2) {
+//   console.log("Tuesday");
+// } else if (day === 3) {
+//   console.log("Wednesday");
+// } else if (day === 4) {
+//   console.log("Thursday");
+// } else if (day === 5) {
+//   console.log("Friday");
+// } else if (day === 6) {
+//   console.log("Saturday");
+// } else {
+//   console.log("give valid input");
+// }
+
+// Using switch statement
+let day = 3;
+
+switch (day) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  default:
+    console.log("Invalid day");
+}
