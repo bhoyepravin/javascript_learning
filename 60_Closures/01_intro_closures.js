8c1904b15e01e3c7b14e34f4134fc2265dd2f7de
bhoyepravin <bhoyepravin288@gmail.com>
2026-06-22 23:09:43 +0530

    closure

--- 60_Closures/01_intro_closures.js
+++ 60_Closures/01_intro_closures.js
@@ -3,10 +3,29 @@
 // function can return function
 
 
-function myFunction() {
+// function outerFunction() {
+//     function innerFunction() {
+//         console.log("I am inside inner function");
+//     }
+//     return innerFunction;
+// }
 
-    return {a: "1"}
+// const ans = outerFunction();
+// // console.log(ans);
+// ans(); // this is closure
+
+// closure is a function that has access to the parent scope, even after the parent function has closed.
+
+
+
+
+function printFullName(firstName, lastName) {
+    function printName() {
+        console.log(`I am ${firstName} ${lastName}`);
+    }
+    return printName;
 }
 
-const ans = outerFunction();
-console.log(ans);
+const ans = printFullName("John", "Doe");
+// console.log(ans);
+ans(); // this is closure
